07bfb1623f33946e76c65daa2b0a30c96ff50f3c
Ashok Pandey <ashok.pragati@gmail.com>
2022-05-14 13:54:18 -0400

    Cryvesto APP w README_app

--- MC_simulation.py
+++ MC_simulation.py
@@ -9,12 +9,22 @@ import alpaca_trade_api as tradeapi
 
 #%matplotlib inline
 
+
+#do montecarlo simulation for the portfolio slected
+#Arguments:
+# - tickers_crypto: crypto tickers selected
+# - tickers: tickers of the funds slecected
+# - num_of_years_of_data: # of yrs of data to use for the simulation
+# - num_of_years_of_projection: # of years to do the projection for
+# - weight: weightage given to each ticker. when added they must total 1
+
 def run_montecarlo(tickers_crypto, tickers, num_of_years_of_data, num_of_years_of_projection, weight):
     load_dotenv('my_api.env')
     os.getenv('ALPACA_API_KEY')
 
     alpaca_api_key = os.getenv('ALPACA_API_KEY')
     alpaca_secret_key = os.getenv('ALPACA_SECRET_KEY')
+
     curr_year =dt.datetime.now().year
 
 # Create the Alpaca tradeapi.REST object
@@ -44,31 +54,25 @@ def run_montecarlo(tickers_crypto, tickers, num_of_years_of_data, num_of_years_o
     portfolio = pd.concat([p_yahoo, p_alpaca], axis=1).dropna()
 
 # Configure the Monte Carlo simulation to forecast based upon num_of_years_of_data  cumulative returns
-#Setup the WEIGHTS ..
-# Run 500 samples.
-#tickers=['BTC-USD','SPY','VTWO', 'IYW', 'QQQ']
-    #weight= [0.03,.02,.30,.30,.20, .15]
     MC_yrs_sim = MCSimulation(
     portfolio_data=portfolio,
     weights= weight,
-  #  weights=[0.03,.02,.35,.35,.25],
-   # weights=[.25,.25,.25,.25],
-
-    #weights=[0.05,.95],
-    num_simulation=100,
+    num_simulation=100, # to reduce time, chose 100
     num_trading_days=252 * num_of_years_of_projection
 )
-# Review the simulation input data
-# YOUR CODE HERE
-
+    
     MC_yrs_sim.calc_cumulative_return()
 
     MC_yrs_sim.plot_simulation()
 
     MC_yrs_sim.plot_distribution()
 
+    #setup the initial investment amount so we can give the projection based upon this investment amount
+
     initial_investment=10000
     summary_ret = MC_yrs_sim.summarize_cumulative_return()
+
+    #print out the results
     '''print('_______________________________________')
 
     print('Summary statistics from the simulations')
@@ -80,9 +84,16 @@ def run_montecarlo(tickers_crypto, tickers, num_of_years_of_data, num_of_years_o
     ci_upper_ten_cumulative_return = summary_ret[9] * initial_investment
 
 # Print the result of your calculations
-    print('________________________________________________________________________________________')
-
-    print(f'For a Portfolio consisting of {tickers_crypto+tickers} ')
-    print(f'              with weights of {weight},' )
-    print(f"using 95% Confidence Intervals, a current investment of ${initial_investment:,.02f} \nwould return from ${ci_lower_ten_cumulative_return:,.02f} to ${ci_upper_ten_cumulative_return:,.02f} over the next {num_of_years_of_projection} years.")
-    print('________________________________________________________________________________________')
+    print('___________________________________________________________________________________________')
+    print(f'Projected Performance of ${initial_investment:,.02f} investment in the Following Portfolio that You Selected:\n')
+    print('TICKER\t\tINVESTMENT\tWEIGHTAGE')
+    j=0
+    for i in tickers_crypto+tickers:
+        print(f'{i}\t\t{initial_investment*weight[j]:,.02f}\t {weight[j]}')
+        j+=1
+    
+    print('_________________')
+    print('PROJECTED RETURNS')
+    print('_________________')
+    print(f"Using 95% Confidence Intervals, and {num_of_years_of_data} years of historical data, \nyour investment of ${initial_investment:,.02f} would return from ${ci_lower_ten_cumulative_return:,.02f} to ${ci_upper_ten_cumulative_return:,.02f} over the next {num_of_years_of_projection} years.")
+    
--- cryvesto.py
+++ cryvesto.py
@@ -14,9 +14,6 @@ tickers=[]
 tickers_crypto=[]
 weight=[]
 
-#ticker_list = [ 'BTC-USD','ETH-USD','BNB-USD','SOL-USD','AVAX-USD','DOT-USD','DOGE-USD', 'EXIT']
-#etf_list = ['SPY', 'VTWO', 'IYW', 'ARKK', 'QQQ', 'EXIT']
-#etfs=['SPY', 'IYW', 'QQQ', 'RYT', 'IETC', 'IWM', 'BND','ARKK','RPV']
 
 ticker_list = { 'BTC-USD': 'Bitcoin',
                 'ETH-USD': 'Ethereum',
@@ -47,6 +44,8 @@ A SMART WAY TO BUILD YOUR PORTFOLIO
 _____________________________________'''
 print(x)
 
+# Set the start and end dates for fetching the data 
+
 end = dt.date(2022,3,31)
 start = dt.date(end.year- 3, end.month, end.day)
 
@@ -61,8 +60,6 @@ while try_again:
 
     tickers_crypto = list(map(str, input(f"\nPick upto 2 Crypto Symbols (Hint- enter tickers space-seperated like ETH-USD BTC-USD) from this list (EXIT to quit): ").split()))
     
-    #tickers_crypto = list(map(str, input(f"For your Portfolio, pick a max of 2 Cryptos from the following list:\n{ticker_list[:-1]}, EXIT to exit): ").split()))
-#    if tickers_crypto in exit_str:
     if tickers_crypto[0] in exit_str:
         print('Thank you for visiting!!')
         exit()
@@ -78,7 +75,7 @@ while try_again:
                 print("Please enter the correct ticker symbol or EXIT to quit")
             break
 
-#Do a Four Factor Analysis on each of the Cryptos selected           
+#Do a Four Factor Analysis on each of the Cryptos selected to find out how is this Crypto behaving           
 for crypto in tickers_crypto:
     ff.ffanalyse(crypto, start, end)
     #ca.analyse(tickers)
@@ -112,8 +109,8 @@ while try_again:
             break
 
     if not try_again:
-        #Do Beta and Sharpe Ratio analysis
-        tc.xact(tickers_crypto, tickers, etf_list, start, end)
+        #Do Risk-Return/Beta and Sharpe Ratio analysis
+        tc.do_risk_return_analysis(tickers_crypto, tickers, etf_list, start, end)
         resp = questionary.text("Type Y to change the ETF selection, ENTER to continue..").ask()
         if resp == 'Y' or resp == 'y':
             try_again=1
@@ -122,12 +119,6 @@ while try_again:
 
 #Get ready for Simulation   
 curr_year =dt.datetime.now().year
-
-start_date = pd.Timestamp(str(curr_year)+'-01-01', tz='America/New_York').isoformat()
-end_date = pd.Timestamp(str(curr_year)+'-01-01', tz='America/New_York').isoformat()
-
-num_of_years_of_data =3
-num_of_years_of_projection = 2
 
 #Now ready to do weightage
 # ask the user to enter the weightage of cryptos and ETFs selected above
@@ -138,19 +129,43 @@ print (f"Now that you have seen the behavior of {tickers_crypto} and how it stac
 print('__________________________________________________________________________________________________________')
 while try_again:
     weight = list(map(float, input(f"Enter weightage (Hint: .05 for 5%, .3 for 30%) for {tickers_crypto+tickers}. Weightage MUST total 1 : " ).split()))
-    
     if len(weight) != len(tickers_crypto+tickers): 
         print ("Please enter weights for all the funds")
         try_again=1
-    elif sum(weight) != float(1):
+    elif sum(weight) != float(1.0):
         print(f'Sum of weights is {sum(weight)}')
         print("Please make sure the sum of all the weights adds upto 1")
         try_again=1
     else:
         try_again=0
+        print('___________________________________________________________________________________________________________')
+
+        print ('''Your projected returns are based upon historical data and our proprietary methodology, 
+if you want to further finetune the analysis, you have the option to select the number of years of historical data.\n''')
+
+        again=1
+        while again:
+            # Give an option to input num of years of data to be used for simulations, default is 3
+            # allow 2-5 years, since Crypto data before 5 years is meaningless (as in 2022)
+            inp = input("How many Years of Historical Data You Want Me To Use (Choose between 2-5)<Press ENTER for 3 years>: ") 
+            if inp=='':
+                num_of_years_of_data =3
+                num_of_years_of_projection = 2
+                again=0
+                break
+            inp=int(inp)
+            if inp > 5 or inp < 2:
+                print ('Uff... 2 - 5 years only, please!')
+                again=1
+            else:
+                num_of_years_of_data = inp
+                num_of_years_of_projection = int(inp/2)
+                again=0
 
         #Run Montecarlo Simulation on the portfolio with the Cryptos and ETFs selected above    
         mc.run_montecarlo(tickers_crypto, tickers, num_of_years_of_data, num_of_years_of_projection, weight)
+
+        #Give option to change the allocation of weightage
         resp = questionary.text("Type Y to change the allocation of the portfolio, ENTER to continue").ask()
         if resp == 'Y' or resp=='y':
             try_again =1
